e9c0067f1187757d01f0ea3d5430ea83a69bceef
Kris <kristy94@bk.ru>
2021-08-20 22:13:57 +0300

    create prompt with questions

---
+++ main.js
@@ -0,0 +1,20 @@
+let numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+console.log(numberOfFilms);
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let firstMovie = prompt("Один из последних просмотренных фильмов?", "");
+let firstRating = prompt("На сколько оцените его?", "");
+let secondMovie = prompt("Один из последних просмотренных фильмов?", "");
+let secondRating = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[firstMovie] = firstRating;
+personalMovieDB.movies[secondMovie] = secondRating;
+
+console.log(personalMovieDB);
